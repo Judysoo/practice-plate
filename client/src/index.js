e0a9b3dfc052a66853f672fe5bc74a38a0378d63
judysoo <judysoo1010@gmail.com>
2020-07-29 15:21:57 +0900

    concurrently 문제 최종 해결

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import rootReducer from './_reducer';
+
+// 보여주고 싶은 컴포넌트 넣기
+
+// promise와 thunk 미들웨어로 객체 외의 function과 promise도 같이 받을 수 있게 함
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk,
+)(createStore);
+// redux store 생성
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/* 보여주고 싶은 컴포넌트 넣기 */}
+  <Provider
+    store={createStoreWithMiddleware(
+      rootReducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__(),
+      //크롬 웹스토어에서 redux devtools 추가
+    )}
+  >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 
