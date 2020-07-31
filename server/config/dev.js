e16b0695ac2192c96b7f3c57690e07928235355c
Judysoo <64236228+Judysoo@users.noreply.github.com>
2020-07-31 14:44:18 +0900

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,5 @@
 //local 환경
 module.exports = {
   mongoURI:
-    'mongodb+srv://JudySoo:admin@firstcluster.3mato.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    'mongodb+srv://JudySoo:admin@firstcluster.3mato.mongodb.net/firstcluster?retryWrites=true&w=majority',
 };
